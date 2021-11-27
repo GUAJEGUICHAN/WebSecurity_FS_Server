25f379a29da36d51180d1753b17ccd30a05019db
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-11-27 20:26:52 +0900

    MongoDB에 아이디 비번 전달 확인

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import { connectDB } from "./database.js";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
+
 const app = express();
 dotenv.config();
 app.use(bodyParser.json());
@@ -25,19 +26,27 @@ app.post("/post", (req, res, next) => {
   console.log(req.body.id);
   console.log(req.body.pw);
   console.log("포스트완료");
+
+  User({id:req.body.id, pw:req.body.pw}).save();
+
   res.status(200).json({ success: true });
+
 });
 
-app.listen(5050, () => {
-  console.log("I'm fine");
-});
+// app.listen(8080, () => {
+//   console.log("I'm fine");
+// });
 
 connectDB().then(()=>{
   console.log("init!")
-  const server = app.listen(process.env.HOST_PORT);
+  const server = app.listen(process.env.HOST_PORT,() => {
+    console.log("I'm fine");
+  });
 }).catch(console.error)
 
 const userSchema = new mongoose.Schema({
   id:{type:String, required:true},
   pw:{type:String, required:true},
 })
+
+const User = mongoose.model('User',userSchema)//유저 컬렉션 사용, 유저스키마 활용
